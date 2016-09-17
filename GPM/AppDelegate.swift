aa84c92bb1c5d6185fba1becacfb20d1a92535cd
mtgto <hogerappa@gmail.com>
2016-09-17 09:38:06 +0900

    Initial Commit

---
+++ GPM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GPM
+//
+//  Created by mtgto on 2016/09/17.
+//  Copyright © 2016 mtgto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GPM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GPM
+//
+//  Created by mtgto on 2016/09/17.
+//  Copyright © 2016 mtgto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
