0570d215f7344394299ad0a3e1b496d0b7c7a751
mtgto <hogerappa@gmail.com>
2016-09-21 09:50:47 +0900

    Ninth commit

--- GPM/KanbanRegisterViewController.swift
+++ GPM/KanbanRegisterViewController.swift
@@ -20,20 +20,26 @@ class KanbanRegisterViewController: NSViewController {
     }
     
     @IBAction func addKanban(_ sender: AnyObject) {
-        if let url = NSURL(string: textField.stringValue), let pathComponents = url.pathComponents {
-            // TODO: validate url
+        if let url = NSURL(string: textField.stringValue), let kanban = self.parseKanbanURL(url) {
+            NotificationCenter.default.post(name: BookmarkViewController.AddKanbanNotificationName, object: kanban)
+            self.dismissViewController(self)
+        } else {
+            // TODO: l10n
+            self.alertTextField.stringValue = "Invalid URL!"
+        }
+    }
+
+    func parseKanbanURL(_ url: NSURL) -> Kanban? {
+        // TODO: validate url
+        if let pathComponents = url.pathComponents {
             let count = pathComponents.count
             if count >= 4 && pathComponents[count - 2] == "projects" {
                 let owner = pathComponents[count - 4]
                 let repo = pathComponents[count - 3]
                 let number = Int(pathComponents[count - 1])!
-                let kanban = Kanban(owner: owner, repo: repo, number: number)
-                NotificationCenter.default.post(name: BookmarkViewController.AddKanbanNotificationName, object: kanban)
-                self.dismissViewController(self)
-                return
+                return Kanban(owner: owner, repo: repo, number: number)
             }
         }
-        // TODO: l10n
-        self.alertTextField.stringValue = "Invalid URL!"
+        return nil
     }
 }
